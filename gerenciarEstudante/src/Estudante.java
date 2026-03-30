afe5008a739825dc8585256f48bb77d30a98b8ab
Pedro <pedro.stefanin08@gmail.com>
2026-03-29 21:05:28 -0300

    do while, static e boolean

--- gerenciarEstudante/src/Estudante.java
+++ gerenciarEstudante/src/Estudante.java
@@ -1,6 +1,9 @@
 public class Estudante {
+  public static final String faculdade= "FATEC Mogi Mirim - Arthur de Azevedo";
   private String nome;
   private String ra;
+  public static String turma = "ADS - Manha";
+  public static int semestre = 3;
   private  double nota1, nota2;
   private double media;
 
@@ -8,16 +11,31 @@ public class Estudante {
         return nome;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    // O metodo .matches() verifica se a String corresponde
+    //  integralmente a um padrão de Expressão Regular (Regex),
+    //  retornando true se o texto seguir o molde ou false caso contrário.
+    public boolean setNome(String nome) {
+        if (nome.matches("[a-zA-Z\\s]+")) {
+            this.nome = nome;
+            return true;
+        } else {
+            System.out.println("Nome invalido! Nao utilize caracteres especiais, apenas letras e espaço");
+            return false;
+        }
     }
 
     public String getRa() {
         return ra;
     }
 
-    public void setRa(String ra) {
-        this.ra = ra;
+    public boolean setRa(String ra) {
+        if (ra.matches("[0-9]+")) {
+            this.ra = ra;
+            return true;
+        } else{
+            System.out.println("RA invalido!");
+            return false;
+        }
     }
 
     public double getNota1() {
@@ -55,9 +73,14 @@ public class Estudante {
         return media;
     }
     public void exibirStatus(){
-        System.out.println("Nome do aluno: "+nome+"\nMedia do aluno: "+media);
+        System.out.println("Nome do aluno: "+nome+"\nRA do aluno: "+ra+"\nMedia do aluno: "+media);
         if (media>=6) System.out.println("Aprovado");
         else System.out.println("Reprovado");
+    }
+    //metodo estatico: Métodos estáticos são funções que você pode chamar sem precisar criar
+    // um aluno com new. Eles pertencem à "ideia" de Estudante, não a um estudante específico.
+    public static void assinaturaFacul(){
+        System.out.println("\nAss: "+faculdade);
     }
 }
 
--- gerenciarEstudante/src/Main.java
+++ gerenciarEstudante/src/Main.java
@@ -1,25 +1,54 @@
 import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+     Estudante e1 = new Estudante();
 
-        Estudante e1 = new Estudante();
+     //settando dados dos atríbutos como dev
+//        e1.setNome("Pedro");
+//        e1.setRa("12345");
+//        e1.setNota1(5.5);
+//        e1.setNota2(8.7);
 
-        e1.setNome("Pedro");
-        e1.setRa("12345");
-        Scanner
-            e1.setNota1(5.5);
+     //settando dados dos atributos como usuário
+        boolean nomeOk;
+        do {
+            System.out.println("Digite o nome do estudante: ");
+            String nomeDigitado = sc.next();
+            nomeOk = e1.setNome(nomeDigitado);
+        } while (!nomeOk);
 
+        boolean raOk;
+        do {
+            System.out.println("Digite o RA do estudante: ");
+            String raDigitado = sc.next();
+            raOk = e1.setRa(raDigitado);
+        } while (!raOk);
 
-        e1.setNota2(8.7);
+        boolean notaOk;
+        do {
+            System.out.println("Digite a nota 1: ");
+            double notaDigitada = sc.nextDouble();
+            notaOk = e1.setNota1(notaDigitada);
+
+        } while(!notaOk);
+
+        do {
+            System.out.println("Digite a nota 2: ");
+            double notaDigitada2 = sc.nextDouble();
+            notaOk = e1.setNota2(notaDigitada2);
+        } while(!notaOk);
+
         e1.calcularMedia();
 
-//                                 Metodo desorganizado
+//                                 Metodo desorganizado de imprimir
 //        System.out.println("Nome do aluno: "+e1.getNome()+", RA: "+e1.getRa()+
 //                ", nota1: "+e1.getNota1()+", nota2: "+e1.getNota2()+", media: "+e1.getMedia());
 
-        //Metodo correto e limpo
+        //Metodo correto e limpo de imprimir
         e1.exibirStatus();
+
+        Estudante.assinaturaFacul();
         }
     }
 
